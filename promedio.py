e812e540a6dce46826016831e700cc0bf1cfed66
Yune08 <yunelonser@gmail.com>
2023-09-08 19:18:50 -0500

    Creacion de archivo

---
+++ promedio.py
@@ -0,0 +1,4 @@
+#Realiza un programa en python que calcule el promedio de los estudiantes utilizando el siguiente diccionario:
+ #   'Juan': 3.2, 4.5, 5
+ #  'Maria': 4.2, 3.5, 4.3
+ #   'Pedro': 3.9, 2.5, 4.8
